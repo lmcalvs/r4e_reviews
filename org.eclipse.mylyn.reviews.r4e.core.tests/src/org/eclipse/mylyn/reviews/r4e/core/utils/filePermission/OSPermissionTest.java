e59fc2a2e4f2255c09df48fa1cc8943ab78bf858
aleon <alvsan09@gmail.com>
2011-02-17 12:11:18 -0500

    R4E core test - Junit test minor update for Hudson build

--- org.eclipse.mylyn.reviews.r4e.core.tests/src/org/eclipse/mylyn/reviews/r4e/core/utils/filePermission/OSPermissionTest.java
+++ org.eclipse.mylyn.reviews.r4e.core.tests/src/org/eclipse/mylyn/reviews/r4e/core/utils/filePermission/OSPermissionTest.java
@@ -120,8 +120,8 @@ public class OSPermissionTest extends TestCase {
 		try {
 			// Grant permissions shall be false since the directory does not exist
 			// FIXME: Decide the best way to report it to the user.
-			// assertFalse(perm.grantWritePermission(dirStr + "1"));
-			assertFalse(perm.grantWritePermission(dirStr));
+			assertFalse(perm.grantWritePermission(dirStr + "1"));
+			// assertTrue(perm.grantWritePermission(dirStr));
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail("Exception");
