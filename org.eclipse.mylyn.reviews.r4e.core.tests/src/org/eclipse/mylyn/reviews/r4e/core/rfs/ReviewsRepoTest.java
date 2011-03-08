19e83f7b85e8bebd3a41d50e4eed84e7a2fdbbc5
aleon <alvsan09@gmail.com>
2011-03-08 09:08:36 -0500

    bug 338754: [R4E] Create / Open local review storage Tests update

--- org.eclipse.mylyn.reviews.r4e.core.tests/src/org/eclipse/mylyn/reviews/r4e/core/rfs/ReviewsRepoTest.java
+++ org.eclipse.mylyn.reviews.r4e.core.tests/src/org/eclipse/mylyn/reviews/r4e/core/rfs/ReviewsRepoTest.java
@@ -24,7 +24,6 @@ import junit.framework.TestCase;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.eclipse.jgit.lib.ObjectId;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -46,17 +45,19 @@ public class ReviewsRepoTest {
 			fRootDir += File.separator;
 		}
 
+		File repoDir = new File(fRootDir);
+		boolean exist = ReviewsRFSProxy.isValidRepo(repoDir);
 		// point to location
-		fRepoProx = new ReviewsRFSProxy(new File(fRootDir), true);
+		fRepoProx = new ReviewsRFSProxy(repoDir, !exist);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		// Remove repository
 		if (fRepoProx != null) {
+			fRepoProx.close();
 			File repoDir = fRepoProx.getRepository().getDirectory();
 			FileUtils.deleteDirectory(repoDir);
-			fRepoProx.close();
 		}
 	}
 
@@ -67,7 +68,7 @@ public class ReviewsRepoTest {
 	public void testRegisterReviewBlobByteArray() {
 		String strContent = new String("The Content");
 		byte[] content = strContent.getBytes();
-		ObjectId id = null;
+		String id = null;
 		InputStream is = null;
 		try {
 			id = fRepoProx.registerReviewBlob(content);
@@ -86,6 +87,116 @@ public class ReviewsRepoTest {
 			fail("Exception");
 		}
 	}
+
+	// /**
+	// * Manual test case: debug settings: unchecked Run on UI thread and continue running after test completion
+	// */
+	// @Test
+	// public void triggerEclipseEditor() {
+	//
+	// Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+	// String strContent = new String(
+	// "package org.eclipse.mylyn.reviews.r4e.core.rfs;/nimport static org.junit.Assert.fail;\npublic class ReviewsRepoTest {}");
+	// byte[] content = strContent.getBytes();
+	// String id = null;
+	// IStorage is = null;
+	// try {
+	// id = fRepoProx.registerReviewBlob(content);
+	// fRepoProx.registerReviewBlob(new String("The Content 2").getBytes());
+	// R4EFileVersion fileVersion = RModelFactory.eINSTANCE.createR4EFileVersion();
+	// fileVersion.setLocalVersionID(id);
+	// fileVersion.setRepositoryPath("/folder/repoDir/fileX.java");
+	// final IFileRevision fileRev = fRepoProx.getIFileRevision(null, fileVersion);
+	//
+	// is = fRepoProx.getIStorage(null, fileVersion);
+	//
+	// Assert.assertNotNull("Istorage \"is\" is null", is);
+	//
+	// final FileRevisionEditorInput editorInp = new FileRevisionEditorInput(id, is);
+	//
+	// // Let a new thread display the action
+	// Thread thread = new Thread(new Runnable() {
+	// public void run() {
+	// Display.getDefault().asyncExec(new Runnable() {
+	// public void run() {
+	// // String editorId = "org.eclipse.jdt.ui.ClassFileEditor";
+	// IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+	// try {
+	// page.openEditor(editorInp, IEditorRegistry.SYSTEM_EXTERNAL_EDITOR_ID);
+	//
+	// } catch (PartInitException e) {
+	// e.printStackTrace();
+	// }
+	// System.out.println("executing");
+	// }
+	// });
+	// }
+	// });
+	// thread.start();
+	// thread.setPriority(Thread.MAX_PRIORITY);
+	//
+	// try {
+	// // Wait for the other thread to execute
+	// Thread.yield();
+	// Thread.sleep(10000);
+	// } catch (InterruptedException e) {
+	// e.printStackTrace();
+	// fail("Interrupted while waiting");
+	// }
+	// } catch (ReviewsFileStorageException e) {
+	// e.printStackTrace();
+	// fail("Exception");
+	// }
+	// }
+	//
+	// /**
+	// * Manual test case: debug settings: unchecked Run on UI thread and continue running after test completion
+	// *
+	// * Trigger Eclipse compare populated with one file only, from local git repo.
+	// */
+	// @Test
+	// public void triggerEclipseCompare() {
+	// Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+	// String strContent = new String(
+	// "package org.eclipse.mylyn.reviews.r4e.core.rfs;/nimport static org.junit.Assert.fail;\npublic class ReviewsRepoTest {}");
+	// byte[] content = strContent.getBytes();
+	// String id = null;
+	// try {
+	// id = fRepoProx.registerReviewBlob(content);
+	// R4EFileVersion fileVersion = RModelFactory.eINSTANCE.createR4EFileVersion();
+	// fileVersion.setLocalVersionID(id);
+	// fileVersion.setRepositoryPath("/folder/repoDir/fileX.java");
+	// final IFileRevision fileRev = fRepoProx.getIFileRevision(null, fileVersion);
+	//
+	// // Let a new thread display the action
+	// Thread thread = new Thread(new Runnable() {
+	// public void run() {
+	// Display.getDefault().asyncExec(new Runnable() {
+	// public void run() {
+	// IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+	// ScmUi.openCompareEditor(page, null, fileRev);
+	// System.out.println("executing");
+	// }
+	// });
+	// }
+	// });
+	//
+	// thread.start();
+	// thread.setPriority(Thread.MAX_PRIORITY);
+	//
+	// try {
+	// // Wait for the other thread to execute
+	// Thread.yield();
+	// Thread.sleep(10000);
+	// } catch (InterruptedException e) {
+	// e.printStackTrace();
+	// fail("Interrupted while waiting");
+	// }
+	// } catch (ReviewsFileStorageException e) {
+	// e.printStackTrace();
+	// fail("Exception");
+	// }
+	// }
 
 	@Test
 	public void testGetRepoRegistry() {
