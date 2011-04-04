d9add71024f6ebf47e9b16c0c92334fe3b000d05
jbouthillie <lmcbout@gmail.com>
2011-04-04 09:53:47 -0400

    Bug 341663 Initial release of LDAP query

---
+++ org.eclipse.mylyn.reviews.r4e.ldap/src/org/eclipse/mylyn/reviews/ldap/internal/util/R4EString.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Ericsson
+ * 
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Description:
+ * 
+ * Contributors:
+ *   Jacques Bouthillier - Initial Implementation of the international string 
+ *******************************************************************************/
+package org.eclipse.mylyn.reviews.ldap.internal.util;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.mylyn.reviews.ldap.Activator;
+
+/**
+ * @author Jacques Bouthillier
+ */
+public class R4EString {
+	// ------------------------------------------------------------------------
+	// Constants
+	// ------------------------------------------------------------------------
+	private static final String			FBUNDLE_NAME		= "org.eclipse.mylyn.reviews.ldap.internal.R4EString";	//$NON-NLS-1$
+
+	private static final ResourceBundle	FRESOURCE_BUNDLE	= ResourceBundle.getBundle(FBUNDLE_NAME);
+
+	// ------------------------------------------------------------------------
+	// Constructors
+	// ------------------------------------------------------------------------
+	/**
+	 * Prevents clients from instantiating this class.
+	 */
+	private R4EString() {
+	}
+
+	// ------------------------------------------------------------------------
+	// Methods
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Gets the key from the value, or 'value' if not found.
+	 * 
+	 * @param value
+	 *            the value to search value.
+	 * @return the key from the value, or 'value' if not found.
+	 */
+	public static String getString(String aKey) {
+		try {
+			return FRESOURCE_BUNDLE.getString(aKey);
+		} catch (MissingResourceException e) {
+			Activator.FTracer.traceWarning("Warning: " + "EXCEPTION In getString for:" + aKey);
+			return '!' + aKey + '!';
+		}
+	}
+
+	/**
+	 * Format the String according to the object pass
+	 * 
+	 * @param aKey
+	 *            String
+	 * @param aArg
+	 *            Object
+	 * @return the key from the value, or 'value' if not found.
+	 */
+	public static String getFormattedString(String aKey, Object aArg) {
+		String format = null;
+		try {
+			format = FRESOURCE_BUNDLE.getString(aKey);
+		} catch (MissingResourceException e) {
+			return "!" + aKey + "!";//$NON-NLS-2$ //$NON-NLS-1$ 
+		}
+		if (aArg == null)
+			aArg = ""; //$NON-NLS-1$ 
+		return MessageFormat.format(format, new Object[] { aArg });
+	}
+
+	/**
+	 * Format the String according to the object pass
+	 * 
+	 * @param aKey
+	 *            String
+	 * @param arg
+	 *            Object array
+	 * @return the key from the value, or 'value' if not found.
+	 */
+	public static String getFormattedString(String aKey, Object[] aArgs) {
+		return MessageFormat.format(FRESOURCE_BUNDLE.getString(aKey), aArgs);
+	}
+
+}
---
+++ org.eclipse.mylyn.reviews.r4e.ldap/src/org/eclipse/mylyn/reviews/userSearch/userInfo/UserInformationFactory.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Ericsson Research Canada
+ * 
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Description:
+ * 
+ * This class represents the factory to get user information.
+ * 
+ * Contributors:
+ *   Jacques Bouthillier - Class to extract User information 
+ *   
+ *******************************************************************************/
+package org.eclipse.mylyn.reviews.userSearch.userInfo;
+
+import org.eclipse.mylyn.reviews.ldap.internal.queryUtility.UserData;
+
+/**
+ * @author Jacques Bouthillier
+ */
+public class UserInformationFactory {
+
+	public static IUserInfo getInstance() {
+		return new UserData();
+	}
+
+}
