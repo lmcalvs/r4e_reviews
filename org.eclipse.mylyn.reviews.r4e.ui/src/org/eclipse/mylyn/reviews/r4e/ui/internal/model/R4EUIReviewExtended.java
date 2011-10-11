3a8fe40cbff15da8651fceeb39460583d1cb1475
sdubois <sebastien.dubois@ericsson.com>
2011-10-11 16:19:55 -0400

    NEW - bug 360584: Allow Update of Time Management properties in REWORK phase https://bugs.eclipse.org/bugs/show_bug.cgi?id=360584

--- org.eclipse.mylyn.reviews.r4e.ui/src/org/eclipse/mylyn/reviews/r4e/ui/internal/model/R4EUIReviewExtended.java
+++ org.eclipse.mylyn.reviews.r4e.ui/src/org/eclipse/mylyn/reviews/r4e/ui/internal/model/R4EUIReviewExtended.java
@@ -659,11 +659,10 @@ public class R4EUIReviewExtended extends R4EUIReviewBasic {
 	 * @return boolean
 	 */
 	public boolean isParticipantTimeSpentEnabled() {
-		if (((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_PREPARATION)
-				|| ((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_DECISION)) {
-			return true;
+		if (((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_COMPLETED)) {
+			return false;
 		}
-		return false;
+		return true;
 	}
 
 	/**
