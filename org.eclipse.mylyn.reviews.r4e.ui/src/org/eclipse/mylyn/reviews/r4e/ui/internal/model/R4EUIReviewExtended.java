68d08b3f9d64c73b4b60488c6eb4e154e929dc4c
sdubois <sebastien.dubois@ericsson.com>
2012-07-10 17:11:01 -0400

    NEW - bug 384631: Unable the update the Decision participant list in REWORK https://bugs.eclipse.org/bugs/show_bug.cgi?id=384631
    Change-Id: Ieff5a0e4987b028d1e4cc40cce1be4979ac2e159
    Signed-off-by: sdubois <sebastien.dubois@ericsson.com>

--- org.eclipse.mylyn.reviews.r4e.ui/src/org/eclipse/mylyn/reviews/r4e/ui/internal/model/R4EUIReviewExtended.java
+++ org.eclipse.mylyn.reviews.r4e.ui/src/org/eclipse/mylyn/reviews/r4e/ui/internal/model/R4EUIReviewExtended.java
@@ -683,7 +683,8 @@ public class R4EUIReviewExtended extends R4EUIReviewBasic {
 	 * @return boolean
 	 */
 	public boolean isDecisionDateEnabled() {
-		if (((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_DECISION)) {
+		if (((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_DECISION)
+				|| ((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_REWORK)) {
 			return true;
 		}
 		return false;
