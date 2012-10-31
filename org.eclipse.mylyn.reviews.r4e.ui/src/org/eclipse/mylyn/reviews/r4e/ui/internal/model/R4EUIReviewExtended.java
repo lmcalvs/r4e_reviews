47d9ab9c4ce11d61c3fe7eb08ef06aeeb571ef30
sdubois <sebastien.dubois@ericsson.com>
2012-10-31 10:12:39 -0400

    364643: Update / Modify participants properties after Preparation (formal reviews)
    Change-Id: Ia6618436a01a24110d68cdbff21febd2115e4d60
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=364643
    Signed-off-by: sdubois <sebastien.dubois@ericsson.com>

--- org.eclipse.mylyn.reviews.r4e.ui/src/org/eclipse/mylyn/reviews/r4e/ui/internal/model/R4EUIReviewExtended.java
+++ org.eclipse.mylyn.reviews.r4e.ui/src/org/eclipse/mylyn/reviews/r4e/ui/internal/model/R4EUIReviewExtended.java
@@ -646,11 +646,10 @@ public class R4EUIReviewExtended extends R4EUIReviewBasic {
 	 * @return boolean
 	 */
 	public boolean isParticipantExtraDetailsEnabled() {
-		if (((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_STARTED)
-				|| ((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_PREPARATION)) {
-			return true;
+		if (((R4EReviewState) fReview.getState()).getState().equals(R4EReviewPhase.R4E_REVIEW_PHASE_COMPLETED)) {
+			return false;
 		}
-		return false;
+		return true;
 	}
 
 	/**
